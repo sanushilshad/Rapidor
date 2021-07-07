071672be1181d90ca9d7289ce24690e58f6e4945
sanu <sanushilshad@gmail.com>
2021-07-07 16:58:42 +0530

    The delete button created

---
+++ Assignment-4/rapidorintro/customer/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path
+from django.urls.conf import include
+from . import views
+
+
+urlpatterns = [
+    path('create', views.create_customer),
+    path('fetch',views.fetch_customer),
+    path('fetch_single',views.fetch_single_customer),
+    path('update',views.update),
+    path('delete',views.delete),
+    # path('main',views.create_cust),
+    path('a/',views.blah)
+
+    
+]
