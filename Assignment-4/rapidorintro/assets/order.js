a43b662a0be356cae6db6118fc72d1db2fd8ec51
sanu <sanushilshad@gmail.com>
2021-07-14 16:44:27 +0530

    Search function issue fixed

--- Assignment-4/rapidorintro/assets/order.js
+++ Assignment-4/rapidorintro/assets/order.js
@@ -1,24 +1,24 @@
-$('#search').on('input', function(){
+$('#search').keyup( function(){
+    
     console.log("Entered: " + $('#search').val());
-
+    
     axios.post('http://127.0.0.1:8000/order/search', {
       customer:  $('#search').val()
 
     })
     .then(function (response) {
-      
+      $('.options').remove()
       console.log(response.data.message);
       console.log(response.data.status);
       console.log(response.data.list);
       let  search_list = response.data.list;
       $.each(search_list, function(index, value) {
         console.log(value);
-        $("#datalistOptions").append("<option value="+value+">");
-        // Will stop running after "three"
+        
+        $("#datalistOptions").append(`<option class='options' value=${value}>`);
+       
         });
-    //   $("option").append("<li>Appended item</li>");
-
-     
     
     })
 })
+
