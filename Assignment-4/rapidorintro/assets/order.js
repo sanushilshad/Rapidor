e9081251689b906141bfb8861c503f8c26f87d1f
sanu <sanushilshad@gmail.com>
2021-07-14 16:48:36 +0530

    Search function issue fixed-2

--- Assignment-4/rapidorintro/assets/order.js
+++ Assignment-4/rapidorintro/assets/order.js
@@ -1,6 +1,8 @@
+// query for the searcch function
 $('#search').keyup( function(){
     
     console.log("Entered: " + $('#search').val());
+    
     
     axios.post('http://127.0.0.1:8000/order/search', {
       customer:  $('#search').val()
@@ -8,12 +10,9 @@ $('#search').keyup( function(){
     })
     .then(function (response) {
       $('.options').remove()
-      console.log(response.data.message);
-      console.log(response.data.status);
-      console.log(response.data.list);
       let  search_list = response.data.list;
       $.each(search_list, function(index, value) {
-        console.log(value);
+
         
         $("#datalistOptions").append(`<option class='options' value=${value}>`);
        
