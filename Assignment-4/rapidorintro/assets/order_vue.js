ae29ada4cbb51598ac78ccdcccc9b0f40c512503
sanu shilshad <sanushilshad@gmail.com>
2021-07-25 02:41:58 +0530

    the order page has been converted to vuejs format

--- Assignment-4/rapidorintro/assets/order_vue.js
+++ Assignment-4/rapidorintro/assets/order_vue.js
@@ -16,18 +16,12 @@ var app = new Vue ({
         gross_total: 0,
         tax_total: 0,
         discount_total: 0,
-        grand_total: 0
+        grand_total: 0,
+        show:false
         
 
     },
     methods: {
-
-        quant_fix(a) {
-            if (isNaN(a) || a == "" || !a) {
-                return a = 0
-            }
-            
-        },
 
         search_value(){
             let url = '/order/search';
@@ -59,28 +53,13 @@ var app = new Vue ({
         },
 
 
-        
-        // checked(code){
-        //     if (this.checked_list.indexOf(code) == -1){
-        //         console.log("checked");
-        //         this.checked_list.push(code);
-        //         console.log(this.checked_list);
-        //       }
-        //     else{
-        //         console.log("unchecked");
-        //         this.checked_list = jQuery.grep(this.checked_list, function(value) {
-        //             return value != code;
-        //           });
-        //           console.log(this.checked_list);
-        //     }
-            
-            
-        // },
+  
 
 
 
         add_product() {
 
+            this.show=true
             this.product_list.forEach((product, index) => {
                 console.log("blag",product.code)
                 if (product.checked && !this.checked_list.includes(product.code)) {
@@ -99,65 +78,68 @@ var app = new Vue ({
             
             
         },
-        delete_product(delete_code){
-                this.order_lines = this.order_lines.filter(order => order.code !== delete_code)
-                this.checked_list = this.checked_list.filter(check => check !== delete_code)
+        delete_product(delete_code) {
+            console.log("delete_code", delete_code);
+            this.order_lines = this.order_lines.filter(order => order.code !== delete_code);
+            this.checked_list = this.checked_list.filter(check => check !== delete_code);
+            delete this.line_total[delete_code];
+            this.all_calculations();
+            console.log("length_line_total:",this.checked_list.length)
+            if (this.checked_list.length == 0) {
+                this.show=false
+            }
+                
+            
         },
 
-        all_calculations(a) {
+        all_calculations() {
             this.gross_total = 0
             this.grand_total = 0
-
-            // this.order_lines.forEach((product, index) => {
-            //     if (isNaN(product['qty'])  ) {
-                    
-            //         product['qty']=0
-                    
-            //     }
-                
-
-                
-            // })
-            // // Calculating line_total 
-            // if (isNaN(a['qty']) || a['qty'] == "") {
-                let gross_amount = parseFloat(a['qty']) * parseFloat(a['unit_price'])
-                let discount_value = (gross_amount * (this.bill_discount / 100))
-                let discounted_amount = gross_amount - discount_value
-                this.line_total[a['code']] = discounted_amount + ((parseFloat(a['tax_percent']) / 100) * discounted_amount)
+            this.tax_total = 0
+            this.discount_total = 0
+            // all_calculations(a) {
+                // let gross_amount = parseFloat(a['qty']) * parseFloat(a['unit_price'])
+                // let discount_value = (gross_amount * (this.bill_discount / 100))
+                // let discounted_amount = gross_amount - discount_value
+                // let tax_value = ((parseFloat(a['tax_percent']) / 100) * discounted_amount)
+                // this.line_total[a['code']] = discounted_amount + tax_value
+                // this.tax_total+=tax_value
             // }
             
             this.order_lines.forEach((product, index) => {
+                
                 let quantity = product.qty
                 let unit_price = product.unit_price
                 quantity = quantity || 0
                 unit_price = unit_price || 0
+
+
+                let gross_amount = parseFloat(quantity) * parseFloat(unit_price)
+                let discount_value = (gross_amount * (this.bill_discount / 100))
+                let discounted_amount = gross_amount - discount_value
+                let tax_value = ((parseFloat(product['tax_percent']) / 100) * discounted_amount)
+                this.line_total[product['code']] = discounted_amount + tax_value
+                //tax_total
+                this.tax_total += tax_value
+
+                //discount_total
+                this.discount_total += discount_value
+                
+
+               //gross_total
                 this.gross_total += (parseFloat(quantity) * parseFloat(unit_price))
-                if (quantity) {
-                    
-                }
-                   
-                    
+
+                // grand_total
+                this.grand_total += this.line_total[product.code]
                     
 
                 
             })
-            //  this.grand_total = (Object.values(this.line_total)).reduce((a, b) => a + b, 0);
-            
-            
+            this.discount_total = this.discount_total.toFixed(2);
+            this.gross_total = this.gross_total.toFixed(2);
+            this.tax_total = this.tax_total.toFixed(2);
+            this.grand_total = this.grand_total.toFixed(2);
 
-
-            // // //Calculating and gross total
-            // // this.gross_total += gross_amount
-
-            // // //Calculating tax_total
-            // // let tax_amount = discounted_amount * (tax_rate/100)
-            // // this.tax_total += tax_amount
-
-            // // //Calculating total discount_total
-            // // this.discount_total += (gross_amount * (this.bill_discount / 100))
-            
-            // // //Calculating grand total
-            // // this.grand_total += line_total
 
         },
         line_total_calc(product_code){
@@ -166,21 +148,40 @@ var app = new Vue ({
                 console.log(this.line_total[product_code])
                 return (this.line_total[product_code])
         },
-        
 
+        order_submit() {
+
+
+            let lines = []
+            this.order_lines.forEach((product, index) => {
+                let product_list = {}
+                product_list['product_name'] = product['name']
+                product_list['product_code'] = product['code']
+                product_list['qty'] = product['qty']
+                product_list['unit_price'] = product['unit_price']
+                product_list['tax_rate'] = product['tax_percent']
+                lines.push(product_list)
+
+            })
+            console.log("blag")
+            axios.post('http://127.0.0.1:8000/order/create', {
+                "customer_code": this.search,
+                "lines": lines,
+            })
+                .then(function (response) {
+                    new Noty({
+                        text: response.data.message,
+                        theme: 'mint',
+                        type: 'success',
+                        timeout: false,
+                    }).show();
+                })
+        }
+        
+            
 
     },
 
-    filters: {
-  cap: function (value) {
-            if (!value) return 0
-            else {
-                return
-            }
-    
-  }
-},
-    
 
 
     mounted (){
